62ab96f8356abb0471a172dcccad47b3b8c1e3ba
vijaysudarsanam98 <51353941+vijaysudarsanam98@users.noreply.github.com>
2022-09-11 10:02:19 +0530

    remove_unused

--- server/models/answers.js
+++ server/models/answers.js
@@ -11,33 +11,3 @@ module.exports.createAnswer = async function (answer,userId,questionId) {
     }
 }
 
-module.exports.questionsGetById = async function (id) {
-    let sqlQuery = 'SELECT id,question FROM users WHERE is_disabled = false AND id = $1';
-    let response = await db.executeSql(sqlQuery, [id]);
-    if (response.rows != null && response.rows !== undefined) {
-            return response.rows[0]
-    } else {
-            return '';
-    }
-}
-
-
-module.exports.deleteQuestion = async function (id) {
-    let sqlQuery = 'UPDATE QUESTIONS SET is_disabled=true where id=$1';
-    let response = await db.executeSql(sqlQuery, [id]);
-    if (response.rows != null && response.rows !== undefined) {
-            return response.rows[0]
-    } else {
-            return '';
-    }
-}
-
-module.exports.checkExistsQuestion = async function (question) {
-    let sqlQuery = 'SELECT id FROM questions WHERE is_disabled = false AND question = $1';
-    let response = await db.executeSql(sqlQuery, [question]);
-    if (response.rows[0] != null && response.rows[0] !== undefined) {
-            return 'question already exists';
-    } else {
-            return '';
-    }
-}
