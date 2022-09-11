e40cad1568cc9fdaf56343641a4cf0d11268ca81
vijaysudarsanam98 <51353941+vijaysudarsanam98@users.noreply.github.com>
2022-09-11 10:08:32 +0530

    doucment_fix

--- server/models/questions.js
+++ server/models/questions.js
@@ -1,9 +1,9 @@
-const db=require('../services/db')
+const db = require('../services/db')
 
 
-module.exports.createQuestion = async function (question,userId) {
+module.exports.createQuestion = async function (question, userId) {
     let insertQuery = 'INSERT INTO QUESTIONS(question,userid) VALUES($1, $2) RETURNING id';
-    let response = await db.executeSql(insertQuery, [question,userId]);
+    let response = await db.executeSql(insertQuery, [question, userId]);
     if (response.rows != null && response.rows !== undefined && response.rows.length > 0) {
         return response.rows[0].id;
     } else {
@@ -15,9 +15,9 @@ module.exports.questionsGetById = async function (id) {
     let sqlQuery = 'SELECT id,question FROM users WHERE is_disabled = false AND id = $1';
     let response = await db.executeSql(sqlQuery, [id]);
     if (response.rows != null && response.rows !== undefined) {
-            return response.rows[0]
+        return response.rows[0]
     } else {
-            return '';
+        return '';
     }
 }
 
@@ -26,9 +26,9 @@ module.exports.deleteQuestion = async function (id) {
     let sqlQuery = 'UPDATE QUESTIONS SET is_disabled=true where id=$1';
     let response = await db.executeSql(sqlQuery, [id]);
     if (response.rows != null && response.rows !== undefined) {
-            return response.rows[0]
+        return response.rows[0]
     } else {
-            return '';
+        return '';
     }
 }
 
@@ -36,8 +36,8 @@ module.exports.checkExistsQuestion = async function (question) {
     let sqlQuery = 'SELECT id FROM questions WHERE is_disabled = false AND question = $1';
     let response = await db.executeSql(sqlQuery, [question]);
     if (response.rows[0] != null && response.rows[0] !== undefined) {
-            return 'question already exists';
+        return 'question already exists';
     } else {
-            return '';
+        return '';
     }
 }
